ad82d7aa6ec83124e662c613856e06a87bf57699
HPW-dev <hpwdev0@gmail.com>
2022-11-08 18:23:47 +0300

    фикс флагов

--- bin/scripts/TVsim2/tvsim 2 BW CRT AM 1.bat
+++ bin/scripts/TVsim2/tvsim 2 BW CRT AM 1.bat
@@ -1,6 +1,6 @@
 set batch_path=%~dp0
 set /a rand_name=%random% %% 100000
-set opts= -w 469 -h 282 --desat_type 1 --scale_type_in 2 --scale_type_out 3 --hfront 0 --hback 0 --hsync_sz 71 --vfront 0 --vback 0 --vsync_sz 6571 --vsync_needed_cnt 1000 --beam_spd_x 1 --beam_spd_y 1 --beam_spd_back 10 --white_lvl 1.18 --black_lvl 0 --beam_off_signal 0 --sync_lvl -0.43 --sync_signal -1.27 --fading 0.3 --pre_amp 1 --amp 1.092 --filter_type 2 --filter_power 3 --noise_level 0 --am_freg 1.00052 --am_depth 1.0875 --am_tune 1 --fix_opts --interlacing --use_fading --use_scale --use_am
+set opts= -w 469 -h 282 --desat_type 1 --scale_type_in 2 --scale_type_out 3 --hfront 0 --hback 0 --hsync_sz 71 --vfront 0 --vback 0 --vsync_sz 6571 --vsync_needed_cnt 1000 --beam_spd_x 1 --beam_spd_y 1 --beam_spd_back 10 --white_lvl 1.18 --black_lvl 0 --beam_off_signal 0 --sync_lvl -0.43 --sync_signal -1.27 --fading 0.3 --pre_amp 1 --amp 1.092 --filter_type 2 --filter_power 3 --noise_level 0 --am_freg 1.00052 --am_depth 1.0875 --am_tune 1 --fix_opts 1 --interlacing 1 --use_fading 1 --use_scale 1 --use_am 1
 "%batch_path%..\..\seze.exe" -i %1 -o %1-%rand_name%.avi ^
 --plug "%batch_path%..\..\plugins\windows\free\tvsim2bw.dll" ^
 --opts "%opts%"
--- bin/scripts/TVsim2/tvsim 2 BW CRT slow fade.bat
+++ bin/scripts/TVsim2/tvsim 2 BW CRT slow fade.bat
@@ -1,6 +1,6 @@
 set batch_path=%~dp0
 set /a rand_name=%random% %% 100000
-set opts=-w 214 -h 192 --desat_type 1 --scale_type_in 2 --scale_type_out 2 --hfront 0 --hback 0 --hsync_sz 300 --vfront 0 --vback 0 --vsync_sz 6571 --vsync_needed_cnt 1000 --beam_spd_x 1 --beam_spd_y 1 --beam_spd_back 10 --white_lvl 2 --black_lvl -0.96 --beam_off_signal -0.96 --sync_lvl -0.96 --sync_signal -1.15 --fading 0.02 --pre_amp 0.807 --amp 0.976 --filter_type 1 --filter_power 2 --noise_level 0 --am_freg 0.99627 --am_depth 1.0775 --am_tune 1 --fix_opts --interlacing --use_fading --use_scale --use_am
+set opts=-w 214 -h 192 --desat_type 1 --scale_type_in 2 --scale_type_out 2 --hfront 0 --hback 0 --hsync_sz 300 --vfront 0 --vback 0 --vsync_sz 6571 --vsync_needed_cnt 1000 --beam_spd_x 1 --beam_spd_y 1 --beam_spd_back 10 --white_lvl 2 --black_lvl -0.96 --beam_off_signal -0.96 --sync_lvl -0.96 --sync_signal -1.15 --fading 0.02 --pre_amp 0.807 --amp 0.976 --filter_type 1 --filter_power 2 --noise_level 0 --am_freg 0.99627 --am_depth 1.0775 --am_tune 1 --fix_opts 1 --interlacing 1 --use_fading 1 --use_scale 1 --use_am 1
 "%batch_path%..\..\seze.exe" -i %1 -o %1-%rand_name%.avi ^
 --plug "%batch_path%..\..\plugins\windows\free\tvsim2bw.dll" ^
 --opts "%opts%"
