b8354aaad23c233e6bcfd9c29b26586d7a17ce8d
HPW-dev <hpwdev0@gmail.com>
2023-10-18 20:26:15 +0300

    доп палитры

---
+++ bin/scripts/gradient-map/gradient-map-icengold.bat
@@ -0,0 +1,5 @@
+set batch_path=%~dp0
+set /a rand_name=%random% %% 100000
+"%batch_path%..\\..\seze.exe" -i %1 -o %1-%rand_name%.avi ^
+--plug "%batch_path%..\\..\plugins\\windows\\free\\gradient_map.dll" ^
+--opts "-p "%batch_path%..\\..\\palettes\\gradient-map\\icengold.png""
